92bb5c5230cd23251eeb1335a227469207a5a165
Gustavo Schneider <schneider@onlab.org>
2022-02-20 13:29:22 -0400

    Adding CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_versioning import VersionedFastAPI
+
 from .config import settings
 from . import api
 
-MIDDLEWARES = []
+origins = [
+    'http://localhost',
+    'http://localhost:8000',
+    'http://localhost:4200'
+]
+
+MIDDLEWARES = [
+    Middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=['*'], allow_headers=['*']),
+]
 
 app = FastAPI(
     title = f'{settings.app_name} version {settings.app_version}',
